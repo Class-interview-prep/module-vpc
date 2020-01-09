38673ab3be50c07517faa7c83869acda1a7c6f7c
Huseyin Akten <husakten@gmail.com>
2020-01-09 15:34:43 -0600

    outputs edited

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 output "vpc-id" {
-  value = "aws_vpc.vpc.id"
+  value = "${aws_vpc.vpc.id}"
 }
 
 output "vpc-cidr" {
-  value = "aws_vpc.vpc.cidr_block"
+  value = "${aws_vpc.vpc.cidr_block}"
 }
 
