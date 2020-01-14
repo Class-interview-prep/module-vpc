9bd8dc1f9250d70b8f1a569932a8f6812a68306e
Huseyin Akten <husakten@gmail.com>
2020-01-14 02:28:00 -0600

    module added

--- vpc-module/carcar/bastion2.tf
+++ vpc-module/carcar/bastion2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "bastion2" {
   instance_type          = "t2.micro"
   ami                    = "ami-02eac2c0129f6376b"
-  subnet_id              = module.vpc.public_subnets[2]
-  key_name               = aws_key_pair.deployer22.key_name
+  subnet_id              = "subnet-03a009b4834ccd036"
+  key_name               = "deployer"
   
   #user_data = file("")
   
