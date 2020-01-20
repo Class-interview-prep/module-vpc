3395284cf584974c7f82e68bc2b070f7bbe3f09f
Huseyin Akten <husakten@gmail.com>
2020-01-19 21:52:01 -0600

    backend removed

--- module/module-vpc.tf
+++ module/module-vpc.tf
@@ -11,20 +11,3 @@ public_subnets  = "${var.public_subnets}"
 private_subnets = "${var.private_subnets}"
 
 }
-
-
-output "vpc_id" {
-  value = "${module.vpc.vpc_id}"
-}
-
-output "vpc_cidr" {
-  value = "${module.vpc.vpc_cidr}"
-}
-
-output "public_subnets" {
-  value = "${module.vpc.public_subnets}"
-}
-
-output "private_subnets" {
-  value = "${module.vpc.private_subnets}"
-}
